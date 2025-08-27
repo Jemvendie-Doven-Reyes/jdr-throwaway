0fc82e862ddbec24bfef4769bbb6f08ae9eb681e
Jemvendie-Doven-Reyes <jemvendiedoven@gmail.com>
2025-08-27 15:58:55 +0800

    firebase configuration

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-       J D R GOALS
+       J D S GOALS
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHvvxAN2aj-MsA1GQsYQluUQsBpiTQJnM",
+  authDomain: "jdr-goal.firebaseapp.com",
+  projectId: "jdr-goal",
+  storageBucket: "jdr-goal.firebasestorage.app",
+  messagingSenderId: "732569243525",
+  appId: "1:732569243525:web:461d0369ef65ce36db3eec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
